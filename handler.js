b1e5bf195f0b7efe134b1730aa7d19304e10bfc3
Fernando Medina Corey <fmcorey@gmail.com>
2017-06-22 13:33:46 -0700

    Working version

--- handler.js
+++ handler.js
@@ -1,18 +1,47 @@
 'use strict';
 
-// AWS Boto setup
+module.exports.sendReminder = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
 
-// AWS SES setup
+    var emailHtml = fs.readFileSync('./reminder.html', 'utf-8');
 
-// AWS Email template setup
-// Handlebars?
+    var params = {
+        Destination: {
+            ToAddresses: ["fmcorey@gmail.com"]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: ["fmcorey@gmail.com"],
+        Source: "fmcorey@gmail.com", 
+    };
 
-// 
-
-module.exports.run = (event, context) => {
-  const time = new Date();
-  console.log(`The serverless SES reminder cron function "${context.functionName}" ran at ${time}`);
-  // send email with populated fields
-
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
 };
+
+
+
+
 
