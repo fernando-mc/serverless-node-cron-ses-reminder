f9ed854b06ba8c70674aea09bb018299b154926b
Fernando Medina Corey <fmcorey@gmail.com>
2017-09-01 01:02:33 -0700

    daily and weekend reminder functions

--- handler.js
+++ handler.js
@@ -1,13 +1,14 @@
 'use strict';
 
-module.exports.sendReminder = (event, context, callback) => {
+module.exports.sendReminderDaily = (event, context, callback) => {
     
     var AWS = require('aws-sdk');
     AWS.config.update({region:'us-east-1'});
     var ses = new AWS.SES();
     var fs = require('fs');
 
-    var emailHtml = fs.readFileSync('./reminder.html', 'utf-8');
+    var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
     var toAndFromAdress = 'verifiedemail@yourdomain.com'
     var params = {
         Destination: {
@@ -22,6 +23,48 @@ module.exports.sendReminder = (event, context, callback) => {
                 Text: {
                     Charset: "UTF-8", 
                     Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress, 
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    var toAndFromAdress = 'verifiedemail@yourdomain.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Here's a weekend Remember that puppies are adorable!!"
                 }
             }, 
             Subject: {
